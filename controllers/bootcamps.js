153b9db1e590e63ebd019232c4cfe70794e0256e
Mahaldi Wijaya Saputra <mahaldi.wijaya@dansmultipro.com>
2023-06-08 20:01:43 +0700

    update learning

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,29 +1,61 @@
 const Bootcamp = require("../models/Bootcamp")
 
 
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    message: 'OK',
-    hello: req.hello
-  })
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find()
+
+    res.status(200).json({
+      success: true,
+      message: 'OK',
+      data: bootcamps
+    })
+  } catch (error) {
+    res.status(404).json({
+      success: false,
+      messsage: 'no record'
+    })
+  }
 }
 
-exports.getBootcamp = (req, res, next) => {
-  res.status(200).json({
-    cing: `ceboi ${req.params.id}`
-  })
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id)
+
+    if(!bootcamp) { // kalo ga ketemu tapi format id nya bener
+      return res.status(404).json({
+        success: false,
+        messsage: 'not found'
+      })
+    }
+    res.status(200).json({
+      success: true,
+      message: 'OK',
+      data: bootcamp
+    })
+  } catch (error) {
+    res.status(404).json({ // kalo dari format id nya salah dia masuk nya ke sini, walau aneh harusnya kalo ga ketemu balik aja ke sini dari mongodb nya
+      success: false,
+      messsage: 'not found'
+    })
+  }
 }
 
 exports.createBootcamp = async (req, res, next) => {
-  console.log('req.body', req.body)
-  // const bootcamp = await Bootcamp.create(req.body)
-
-  res.status(201).json({
-    success: true,
-    message: 'OK',
-    data: req.body
-  })
+  try {
+    const bootcamp = await Bootcamp.create(req.body)
+  
+    res.status(201).json({
+      success: true,
+      message: 'OK',
+      data: bootcamp
+    })
+    
+  } catch (error) {
+    res.status(400).json({
+      success: false
+    })
+  }
 }
 
 exports.updateBootcamp = (req, res, next) => {
