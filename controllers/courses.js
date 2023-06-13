84fe48569fd129ce6d448b25af1de1804b7c986d
Mahaldi Wijaya Saputra <mahaldi.wijaya@dansmultipro.com>
2023-06-13 16:28:11 +0700

    create, update delete course

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,4 +1,5 @@
 const Course = require("../models/Course")
+const Bootcamp = require("../models/Bootcamp")
 const ErrorResponse = require('../utils/errorResponse')
 const asyncHandler = require('../middleware/async')
 
@@ -20,5 +21,59 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
     data: course,
     total: course.length,
     success: true
+  })
+})
+
+exports.getCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id)
+
+  if(!course) return next(new ErrorResponse(`id not found ${req.params.id}`, 404))
+
+  res.status(200).json({
+    data: course,
+    success: true,
+    message: 'OK'
+  })
+})
+
+exports.createCourse = asyncHandler(async(req, res, next) => {
+ // /bootcamps/5d713995b721c3bb38c1f5d0/courses
+  const bootcamp = await Bootcamp.findById(req.params.bootcampId)
+
+  if(!bootcamp) return next(new ErrorResponse(`id bootcamp not found ${req.params.id}`, 404))
+
+  req.body.bootcamp = req.params.bootcampId
+  const course = await Course.create(req.body)
+
+  res.status(200).json({
+    data: course,
+    success: true,
+    message: 'Created'
+  })
+})
+
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  })
+  if(!course) return next(new ErrorResponse(`id not found ${req.params.id}`, 404))
+
+  res.status(200).json({
+    data: course,
+    success: true,
+    message: 'Updated'
+  })
+})
+
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findOneAndDelete(req.params.id)
+
+  if(!course) return next(new ErrorResponse(`id not found ${req.params.id}`, 404))
+
+  res.status(200).json({
+    data: {},
+    success: true,
+    message: 'Deleted'
   })
 })
--- routes/courses.js
+++ routes/courses.js
@@ -2,11 +2,22 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 
 const {
-  getCourses
+  getCourses,
+  getCourse,
+  createCourse,
+  updateCourse,
+  deleteCourse
 } = require('../controllers/courses')
 
 router
   .route('/')
   .get(getCourses)
+  .post(createCourse)
+
+router
+  .route('/:id')
+  .get(getCourse)
+  .put(updateCourse)
+  .delete(deleteCourse)
 
 module.exports = router;
